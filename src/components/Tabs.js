6af4f241e3d799cd17e25a32003732889c9c1e88
[AckintyaG] <kumar9419pk@gmail.com>
2023-08-29 12:57:05 +0530

    modified

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 const Tabs = ({ tabs }) => {
   const [activeTab, setActiveTab] = useState(0);
 
-<<<<<<< HEAD
   
  function handleTabClick(content){
   setActiveTab(content);
@@ -13,29 +12,10 @@ const Tabs = ({ tabs }) => {
       <ul>
         {tabs.map((value) => 
            <li onClick={() => handleTabClick(value.content)}>{value.title}
-=======
-  const handleTabClick = (content) => {
-    setActiveTab(content);
-  };
-
-  return (
-    <div >
-      <ul>
-        {tabs.map((tab, content) => (
-           <li
-              key={value}
-              className={`tab-item ${value === activeTab ? 'active' : ''}`}
-              onClick={() => handleTabClick(value.content)}
-            >
->>>>>>> 3e61863d9c19249559c0ff9c87f0c97a9e8546f8
               </li>
         )}        
       </ul >
-<<<<<<< HEAD
       {activeTab && <p>This is the content for{activeTab}.</p>}
-=======
-      {setActiveTab && <p>This is the content for{activeTab}</p>}
->>>>>>> 3e61863d9c19249559c0ff9c87f0c97a9e8546f8
     </div >
   );
 };

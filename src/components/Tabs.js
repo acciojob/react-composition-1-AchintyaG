e9b552cb1023fcc265fdb7a3ac8485f28dbfcb91
[AckintyaG] <kumar9419pk@gmail.com>
2023-08-29 11:51:25 +0530

    updated code

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -9,21 +9,18 @@ const Tabs = ({ tabs }) => {
 
   return (
     <div className="tabs-container">
-      <div className="tabs-list">
+      <ul>
         {tabs.map((tab, index) => (
-          <div
-            key={index}
-            className={`tab-item ${index === activeTab ? 'active' : ''}`}
-            onClick={() => handleTabClick(index)}
-          >
-            {tab.title}
-          </div>
-        ))}
-      </div>
-      <div className="tab-content">
-        {tabs[activeTab].content}
-      </div>
-    </div>
+           <li
+              key={index}
+              className={`tab-item ${index === activeTab ? 'active' : ''}`}
+              onClick={() => handleTabClick(index)}
+            >
+              </li>
+        ))}        
+      </ul >
+      {setActiveTab && <p>This is the content for{activeTab.content}</p>}
+    </div >
   );
 };
 
